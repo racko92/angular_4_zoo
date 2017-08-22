016d339a2a9e29540cc1daab73f29c1abf7f7555
racko92 <vladimir.rackovic.92@gmail.com>
2017-08-22 19:14:25 +0200

    Zadatak 7: Kreirana nova klasa 'Sector', klasa Animal prosirena poljem Sector

--- src/app/animals.ts
+++ src/app/animals.ts
@@ -1,13 +1,17 @@
+import { Sector } from './sector';
+
 export class Animal{
     species: string;
     name: string;
     dateOfBirth: string;
+    sector: Sector;
 
-    constructor(species: string, name: string, dateOfBirth: string){
+    constructor(species: string, name: string, dateOfBirth: string, sector?: Sector){
 
         this.species = species;
         this.name = name;
         this.dateOfBirth = dateOfBirth;
+        this.sector = sector;
 
     }
 }
--- src/app/animals/animal-list.component.ts
+++ src/app/animals/animal-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Animal } from '../animals.component';
+import { Animal } from '../animals';
+import { Sector } from '../sector';
 
 @Component({
     selector: 'app-animals',
@@ -8,15 +9,22 @@ import { Animal } from '../animals.component';
 export class AnimalsComponent{
     
         animals: any[];
+        sector: Sector[];
         newAnimal: Animal = new Animal('','','');
     
         constructor(){
+            this.sector = [
+                new Sector('land', 'land'),
+                new Sector('desert', 'desert'),
+                new Sector('woods', 'woods'),
+                new Sector('wather', 'wather')
+            ]
             this.animals = [
-                    new Animal('Dog','John','01.01.2000'),
-                    new Animal('Cat','Meow',''),
-                    new Animal('Hamster','Mickey','01.01.2000'),
-                    new Animal('Spider', 'Spidey', '01.01.2000'),
-                    new Animal('Tiger','Leo', '')
+                    new Animal('Dog','John','01.01.2000', this.sector[0]),
+                    new Animal('Cat','Meow','', this.sector[1]),
+                    new Animal('Hamster','Mickey','01.01.2000', this.sector[2]),
+                    new Animal('Spider', 'Spidey', '01.01.2000', this.sector[3]),
+                    new Animal('Tiger','Leo', '', this.sector[0])
                 ];
     
         }    

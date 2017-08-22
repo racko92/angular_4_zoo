bfccc70be29d9a8cb6098e2b50916c1eedf323a5
racko92 <vladimir.rackovic.92@gmail.com>
2017-08-22 17:47:01 +0200

    Zadatak 4: Dodata klasa Animal i premestena u poseban file i importovana u AnimalList fajl.

---
+++ src/app/animals/animal-list.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Animal } from '../animals.component';
+
+@Component({
+    selector: 'app-animals',
+    templateUrl: './animal-list.component.html'
+})
+export class AnimalsComponent{
+    
+        private animals: any[];
+    
+        constructor(){
+            this.animals = [
+                    new Animal('Dog','John','01.01.2000'),
+                    new Animal('Cat','Meow',''),
+                    new Animal('Hamster','Mickey','01.01.2000'),
+                    new Animal('Spider', 'Spidey', '01.01.2000'),
+                    new Animal('Tiger','Leo', '')
+                ];
+    
+        }    
+        
+        remove(animal){
+            const index = this.animals.indexOf(animal);
+            this.animals.splice(index, 1);
+        }
+    }
